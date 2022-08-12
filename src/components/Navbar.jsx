aae4bdb9d706f695a613d217cbe3c226f7bcf896
wmaKRET <wkret91.2@gmail.com>
2022-08-12 09:41:29 +0200

    Create links to Home/Store pages and btn for shopping cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
+
+import { Button, Container, Nav, Navbar as NavbarBS} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { Basket2 } from "react-bootstrap-icons"
+
 export const Navbar = () => {
     return (
-        <div>
-            navbar
-        </div>
+        <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
+            <Container>
+                <Nav className="me-auto">                    
+                    <Nav.Link to="/" as={NavLink}>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link to="/store" as={NavLink}>
+                        Store
+                    </Nav.Link>
+                </Nav>
+                <Button className="btn-secondary">
+                    <Basket2 size={20}/>
+                </Button>
+            </Container>
+        </NavbarBS>
     )
 }
