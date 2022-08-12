25c7f0b7b57326a90f2146ed0bbeeabdd8c40214
wmaKRET <wkret91.2@gmail.com>
2022-08-12 09:26:51 +0200

    Create files structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
 import React from "react";
-export const App = () => {
-  return <div className="App"></div>;
-};
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
+export const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Container>
+  </>
+);
