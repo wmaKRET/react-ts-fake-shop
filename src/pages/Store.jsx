e1f33059d8524e98508ad9b82128912454cb37a7
wmaKRET <wkret91.2@gmail.com>
2022-08-12 09:47:08 +0200

    Add basic structure and style to Store page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,11 @@
+import { Col, Row } from "react-bootstrap"
 export const Store = () => {
     return (
-        <div>
-            Store
-        </div>
+        <>
+            <h1 className="text-center">Welcome to <span className="font-weight-bold" style={{ letterSpacing: "0.2rem"}}>FAKE SHOP</span>!</h1>
+            <Row xs={1} md={2} lg={3} className="g-2">
+                <Col>Item</Col>
+            </Row>
+        </>
     )
 }
